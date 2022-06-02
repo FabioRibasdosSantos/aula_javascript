1910b1e89b9e20f0db84aa79303618b9e2bc4899
Fabio Ribas dos Santos <fabyo_27@hotmail.com>
2022-06-01 22:23:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var nome = "Fabio Ribas dos Santos";
+var idade = 31;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo " + nome);
+//alert(idade + idade2)
+alert(nome + " tem " + idade + " anos.");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase())
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
